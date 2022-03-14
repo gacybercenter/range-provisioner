b0a91c8b5502166d50b26e4d134d075aecee50a1
Alan Cantrell <alcantrell@augusta.edu>
2022-03-14 01:02:50 -0400

    WIP manage_guac re-write

--- manage_guac.py
+++ manage_guac.py
@@ -7,9 +7,8 @@ from openstack import config, connect, enable_logging
 guac_admin = constants.GUAC_ADMIN
 guac_password = constants.GUAC_ADMIN_PASS
 
+guac_user_password = constants.GUAC_USER_PASSWORD
 
-guac_user_prefix = constants.GUAC_USER_PREFIX
-guac_user_organization = constants.GUAC_USER_ORGANIZATION
 guac_connection_group = constants.GUAC_CONN_GROUP
 
 guac_event_prefix = constants.GUAC_EVENT_PREFIX
@@ -50,14 +49,15 @@ def get_existing_accounts():
 
 
 def get_conn_group_id(conn_group_name):
-    conn_groups = list_conn_groups()
+    conn_groups = loads(guac_session.list_connection_groups())
     group_id = [ key for key in conn_groups.keys() if conn_groups[key]['name'] == conn_group_name and not None ]
     return group_id
 
 
-def list_conn_groups():
-    conn_groups = loads(guac_session.list_connection_groups())
-    return conn_groups
+def get_conn_id(conn_name, conn_group_id):
+    list_conns = loads(guac_session.list_connections())
+    conn_id = ([ k for k,v in list_conns.items() if v.get('parentIdentifier') == conn_group_id and v.get('name') == conn_name ])[0]
+    return conn_id
 
 
 def create_conn_group(conn_group_name):
@@ -75,17 +75,42 @@ def delete_conn_group(conn_group_name, conn_group_id):
     print(f"Guacamole:  The connection group {conn_group_name} with id {conn_group_id} was deleted")
 
 
-def guac_manage_user_acct(guac_action):
-    usernames=create_usernames()
+def create_user_acct(user, guac_user_password, guac_user_organization):
+        guac_session.create_user(f'{user}', guac_user_password, {"guac-organization": guac_user_organization})
+        print(f"Guacamole:  Created User Account: {user}")
 
-    for username in usernames:
-        if guac_action == "create":
-            guac_session.create_user(f'{username}', guac_user_password, {"guac-organization": guac_user_organization})
-            print(f"Guacamole:  Created User Account: {username}")
-        if guac_action == "delete":
-            guac_session.delete_user(username)
-            print(f'Guacamole:  Deleted User Account: {username}')
 
+def delete_user_acct(user):
+        guac_session.delete_user(user)
+        print(f'Guacamole:  Deleted User Account: {user}')
+
+
+def create_user_conns(user, instances, conn_proto, conn_group_id, ostack_username, ostack_pass):
+    for instance in instances:
+    
+        if "ssh" in conn_proto:
+            conn_name = f"{instance['name']}.ssh"
+            guac_session.manage_connection("post", "ssh", conn_name, conn_group_id, None, {"hostname": instance['address'], "port": "22", "username": ostack_username, "password": ostack_pass}, {"max-connections": "", "max-connections-per-user": "2" })
+            print(f"Guacamole:  Created connection for {instance['name']} ssh connection with address: {instance['address']}")
+
+        if "rdp" in conn_proto:
+            conn_name = f"{instance['name']}.rdp"
+            guac_session.manage_connection("post", "rdp", conn_name, conn_group_id, None, {"hostname": instance['address'], "port": "3389", "username": ostack_username, "password": ostack_pass, "security": "any", "ignore-cert": "true"}, {"max-connections": "", "max-connections-per-user": "2" })
+            print(f"Guacamole:  Created connection for {instance['name']} rdp connection with address: {instance['address']}")
+
+
+def associate_user_conns(user, instance, conn_group_id):
+            
+            conn = get_conn_id(conn_name, conn_group_id)
+            guac_session.update_user_connection(user, conn_group_id, "add", True)
+            guac_session.update_user_connection(user, conn, "add", False)
+            print(f"Guacamole:  Associated connections for {user} with id {conn} for server {conn_name}")
+    
+
+
+
+# def delete_user_conns():
+#     blah blah
 
 def guac_manage_user_conns(guac_action, guac_user_conn_proto):
     vconnection = {'name': guac_connection_group}
@@ -109,6 +134,7 @@ def guac_manage_user_conns(guac_action, guac_user_conn_proto):
                     if proto == "ssh":
                         guac_session.manage_connection("post", "ssh", vconnection['guac_instance_name'] + ".ssh", vconnection['group_id'], None, {"hostname": vconnection['instance_address'], "port": "22", "username": ostack_instance_username, "password": ostack_instance_pw}, {"max-connections": "", "max-connections-per-user": "1" })
                         print(f'Guacamole:  Created connection for {vconnection["guac_instance_name"]} ssh connection with address: {vconnection["instance_address"]}')
+
                     if proto == "rdp":
                         guac_session.manage_connection("post", "rdp", vconnection['guac_instance_name'] + ".rdp", vconnection['group_id'], None, {"hostname": vconnection['instance_address'], "port": "3389", "username": ostack_instance_username, "password": ostack_instance_pw, "security": "any", "ignore-cert": "true"}, {"max-connections": "", "max-connections-per-user": "1" })
                         print(f'Guacamole:  Created connection for {vconnection["guac_instance_name"]} rdp connection with address: {vconnection["instance_address"]}')
@@ -131,6 +157,7 @@ def main():
     # Create main dictionary and load templates
     
     guac_dict = {}
+    instance_dict = {}
     heat_params = load_template(main_template)
     global_params = load_template(globals_template)
 
@@ -156,29 +183,56 @@ def main():
     guac_dict.update({'ostack_instance_password':
                      heat_params['parameters']['password']['default']})
     
-    usernames = create_usernames(guac_dict['num_users'], guac_dict['username'])
-    conn_group_id = get_conn_group_id(guac_dict['conn_group_name'])
+    conn_group_id = (get_conn_group_id(guac_dict['conn_group_name']))[0]
+
+    instance_list = conn.list_servers()
+
+    get_conn_id("linux1.server.sec_plus.3.rdp", conn_group_id)
 
 
-# Create/Delete connection groups if specified in globals template
-    if guac_dict['conn_group_action'] == "create":
-        if conn_group_id:
-            print(f"Guacamole ERROR:  Can't create connection group, {guac_dict['conn_group_name']} already exists")
-        else:
-            create_conn_group(guac_dict['conn_group_name'])
-    if guac_dict['conn_group_action'] == "delete":
-            if not conn_group_id:
-                print(f"Guacamole ERROR:  Can't delete connection group, {guac_dict['conn_group_name']} doesn't exist")
-            else:
-                delete_conn_group(guac_dict['conn_group_name'], conn_group_id[0])
-    
+
+
+
+    # ## Create/Delete connection groups if specified in globals template
+    # if guac_dict['conn_group_action'] == "create":
+    #     if conn_group_id:
+    #         print(f"Guacamole ERROR:  Can't create connection group, {guac_dict['conn_group_name']} already exists")
+    #     else:
+    #         create_conn_group(guac_dict['conn_group_name'])
+    # if guac_dict['conn_group_action'] == "delete":
+    #         if not conn_group_id:
+    #             print(f"Guacamole ERROR:  Can't delete connection group, {guac_dict['conn_group_name']} doesn't exist")
+    #         else:
+    #             delete_conn_group(guac_dict['conn_group_name'], conn_group_id)
+
+    # # Create/Delete user accounts if specified in globals template
+    # usernames = create_usernames(guac_dict['num_users'], guac_dict['username'])
+    # for user in usernames:
+    #     if guac_dict['guac_action'] == 'create':
+    #         if user in get_existing_accounts():
+    #             print(f"Guacamole ERROR:  Can't create user account, {user} already exists")
+    #         else:
+    #             create_user_acct(user, guac_user_password, guac_dict['user_org'])
+    #             user_num = f"{user}".rsplit('.', 1)[1]
+    #             instances = [ {'name': instance.name, 'address': instance.public_v4} for instance in instance_list if instance['name'].endswith(f"{guac_dict['conn_name']}.{user_num}") ]
+    #             create_user_conns(user, instances, guac_dict['conn_proto'], conn_group_id, guac_dict['ostack_instance_username'], guac_dict['ostack_instance_password'])
+    #             get_conn_id(guac_dict['conn_name'])
+
+
+    #     if guac_dict['guac_action'] == 'delete':
+    #         if user not in get_existing_accounts():
+    #             print(f"Guacamole ERROR:  Can't delete user account, {user} doesn't exist")
+    #         else:
+    #             delete_user_acct(user)
+
+        
+    # if guac_dict['guacamole']['action'] == "create":
+        
     
     # print(loads(guac_session.list_connection_groups()))
 
 
 
-    # guac_session.create_connection_group('test_group', "ORGANIZATIONAL", "ROOT", {'max-connections': '50', 'max-connections-per-user' : '10', 'enable-session-affinity' : ''})
-    # guac_session.delete_connection_group(16)
 
 
     # print(get_ostack_instances())
