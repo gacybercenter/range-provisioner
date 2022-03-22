6ab640a5faff17d0483101bf2f501c5541ed23d4
Alan Cantrell <alcantrell@augusta.edu>
2022-03-22 23:48:49 +0000

    fix if statement for stack build wait condition

--- manage_heat.py
+++ manage_heat.py
@@ -141,12 +141,9 @@ def main():
             update_stack(f'{heat_param_dict["instance_id"]}',
                          main_template, heat_param_dict)
         if heat_global_dict['main_action'] == 'create':
-            if num == (global_dict['num_users']+1):
+            if num == global_dict['num_users']:
                 create_stack_wait(f'{heat_param_dict["instance_id"]}',
                          main_template, heat_param_dict)
-                print("Waiting for stack resource allocation to finish")
-                time.sleep(30)
-                print("Stack Deployment Complete")
             else:
                 create_stack(f'{heat_param_dict["instance_id"]}',
                             main_template, heat_param_dict)
