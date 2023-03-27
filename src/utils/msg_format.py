ecbe9f5e47cf23cb28d87f98ae68de8258389f1d
Jonathan Race <jrace@augusta.edu>
2023-03-27 19:49:22 +0000

    chore: enhanced messaging, and added in reliable debugging practice along with simplifying template load process

--- src/utils/msg_format.py
+++ src/utils/msg_format.py
@@ -1,13 +1,36 @@
 from colorama import Fore
 
-
 def error_msg(error):
-    print(Fore.RED + "ERROR:  " + Fore.RESET + error)
+    if '\n' in error:
+        # If error contains multiple lines, split it and print each line separately
+        error_lines = error.split('\n')
+        print(Fore.RED + "ERROR: " + Fore.RESET)
+        for line in error_lines:
+            print(line)
+    else:
+        # If error is a single line, print it as is
+        print(Fore.RED + "ERROR: " + Fore.RESET + error)
 
 
 def info_msg(text):
-    print(Fore.BLUE + "INFO:  " + Fore.RESET + text)
+    if '\n' in text:
+        # If text contains multiple lines, split it and print each line separately
+        text_lines = text.split('\n')
+        print(Fore.BLUE + "INFO: " + Fore.RESET)
+        for line in text_lines:
+            print(line)
+    else:
+        # If text is a single line, print it as is
+        print(Fore.BLUE + "INFO: " + Fore.RESET + text)
 
 
 def success_msg(text):
-    print(Fore.GREEN + "SUCCESS:  " + Fore.RESET + text)
+    if '\n' in text:
+        # If text contains multiple lines, split it and print each line separately
+        text_lines = text.split('\n')
+        print(Fore.GREEN + "SUCCESS: " + Fore.RESET)
+        for line in text_lines:
+            print(line)
+    else:
+        # If text is a single line, print it as is
+        print(Fore.GREEN + "SUCCESS: " + Fore.RESET + text)
