bd9284101458825041d9e8c0e14c5d4a6f67de4c
Jonathan Race <jrace@augusta.edu>
2023-03-28 19:20:16 +0000

    chore: reworked swift integration to perform proper checks and add appropriate messaging, also added general debuging to messaging and setup provisioning with arguements

--- src/orchestration/swift.py
+++ src/orchestration/swift.py
@@ -1,109 +1,144 @@
-from openstack import connect
+import json
+from os import path, walk
 from utils.msg_format import error_msg, info_msg, success_msg
-from utils.load_template import load_template, load_global, load_heat
 
 
-
-class SwiftProvision:
-    def __init__(self, parsed_args):
-        if parsed_args.pipeline:
-            self.container_name = global_swift_dict['container_name']
-        if parsed_args.pipeline is False:
-            self.container_name = parsed_args.container_name
-        
-        print(self.container_name)
-
-
-    def search_containers(self):
-        """Search if container exists"""
-        info_msg(f"Searching for {self.container_name} container...")
-        container_exists = conn.search_containers(name=self.container_name)
-        return(container_exists)
-
-
-    def create_container(self):
-        """Create new object store container"""
-        try:
-            container_created = conn.object_store.create_container(name=self.container_name)
-            success_msg(f"Container {self.container_name} has been created"
-                " in the object store")
-            conn.set_container_access(name=self.container_name, access="public")
-        except Exception as e:
-            error_msg(f"Cannot create container, {self.container_name} it already exists \n ({e})")
-            container_created = None
-        return container_created
-
-
-    def delete_container(self):
-        """Delete container from object store"""
-        container = SwiftProvision.search_containers(self)
+def provision(conn, container_name, asset_dir, debug=False):
+    """Provision container and upload assets"""
+    try:
+        container = create(conn, container_name, debug)
         if container:
-            conn.delete_container(self.container_name)
-            success_msg(f"{self.container_name} container has been deleted from the object store")
+            upload_objs(conn, container_name, asset_dir, debug)
         else:
-            error_msg(f"Cannot delete container {self.container_name}, doesn't exist")
+            return None
+    except Exception as e:
+        error_msg(e)
 
 
-def swift_provision(parsed_args):
-        if parsed_args.pipeline:
-            global_swift_dict.update({'container_name': heat_dict.parameters['container_name']['default']})
-            SwiftProvision(global_swift_dict['container_name'], parsed_args).create_container()
-        if parsed_args.pipeline is False:
-            SwiftProvision(global_swift_dict['container_name'], parsed_args).create_container()
+def deprovision(conn, container_name, debug=False):
+    """Deprovision container and delete assets"""
+    try:
+        delete(conn, container_name, debug)
+    except Exception as e:
+        error_msg(e)
 
 
+def search(conn, container_name, debug=False):
+    """Search if container exists"""
+    try:
+        info_msg(f"Searching for {container_name} container...")
+        result = conn.search_containers(name=container_name)
+
+        if result:
+            success_msg(f"{container_name} container exists")
+            return result
+        info_msg(f"{container_name} container doesn't exist")
+        return None
+    except Exception as e:
+        error_msg(e)
 
 
+def create(conn, container_name, debug=False):
+    """Create new object store container"""
+    try:
+        exists = search(conn, container_name, debug)
+        if exists:
+            error_msg(
+                f"Cannot create container, {container_name} it already exists")
+            return None
+        else:
+            info_msg(
+                f"Creating container {container_name} in the object store", debug)
+            container = conn.object_store.create_container(name=container_name)
+            if container:
+                success_msg(f"Container {container_name} has been created")
+                return container
+    except Exception as e:
+        error_msg(e)
 
 
+def delete(conn, container_name, debug=False):
+    """Delete container from object store"""
+    try:
+        exists = search(conn, container_name, debug)
+        if exists:
+
+            delete_objs(conn, container_name, debug)
+
+            info_msg(f"Deleting container... {container_name}", debug)
+            conn.object_store.delete_container(container_name, debug)
+            success_msg(f"{container_name} container has been deleted")
+        else:
+            error_msg(
+                f"Cannot delete container, {container_name} does not exists")
+            return None
+    except Exception as e:
+        error_msg(e)
 
 
+def access(conn, container_name, debug=False):
+    """Set container access to public"""
+    try:
+        container = search(conn, container_name, debug)
+        if container:
+            info_msg(f"Setting container {container_name} to public", debug)
+            access = conn.set_container_access(
+                name=container_name, access="public")
+            if access:
+                success_msg(f"Container {container_name} is now public")
+                return access
+    except Exception as e:
+        error_msg(e)
 
 
-# def upload_objs(container_name, dir):
-#     """Create directory markers and upload objects"""
-#     # Collect all the files and folders in the given directory
-#     objs = []
-#     dir_markers = []
-#     for (_dir, _ds, _fs) in walk(dir):
-#         if not (_ds + _fs):
-#             dir_markers.append(_dir)
-#         else:
-#             objs.extend([path.join(_dir, _f) for _f in _fs])
+def upload_objs(conn, container_name, dir, debug=False):
+    """Create directory markers and upload objects"""
+    try:
+        # Collect all the files and folders in the given directory
+        objs = []
+        dir_markers = []
+        for (_dir, _ds, _fs) in walk(dir):
+            if not (_ds + _fs):
+                dir_markers.append(_dir)
+            else:
+                objs.extend([path.join(_dir, _f) for _f in _fs])
 
-#     # Create directory markers for folder structure
-#     dir_markers = [
-#         conn.create_directory_marker_object(
-#             container_name,d,) for d in dir_markers
-#         ]
+        # Create directory markers for folder structure
+        dir_markers = [
+            conn.create_directory_marker_object(
+                container_name, d,) for d in dir_markers
+        ]
+        success_msg(
+            f"Required directories have been created in the {container_name} container")
 
-#     # Create objects
-#     objs = [
-#         conn.create_object(
-#         container_name, o,) for o in objs
-#         ]
+        # Create objects
+        objs = [
+            conn.create_object(
+                container_name, o,) for o in objs
+        ]
+        success_msg(
+            f"Objects have been uploaded to the {container_name} container")
 
-#     objects = conn.list_objects(container_name)
-
-#     print("Openstack_Swift:  The following objects have been uploaded to the"
-#           f" container {container_name}:")
-#     for obj in objects:
-#         print(f"  - {obj.name}")
-
-
-# def delete_objs(container_name):
-#     """Delete container objects"""
-#     try:
-#         objects = conn.list_objects(container_name)
-#         print("Openstack_Swift:  The following objects were deleted from the"
-#               f" {container_name} container:")
-#         for obj in objects:
-#             conn.delete_object(container_name, str(obj.name))
-#             print(f"  - {obj.name}")
-#     except Exception as e:
-#         print(f"Openstack_Swift ERROR:  Cannot delete container objects"
-#               f" in {container_name} the container doesn't exist")
-#         print(f"Openstack_Swift ERROR:  {e}")
+        objects = conn.list_objects(container_name)
+        if objects:
+            info_msg(f"Listing objects from {container_name}", debug)
+            info_msg(json.dumps(objects, indent=4), debug)
+            for obj in objects:
+                info_msg(f"uploaded:     {obj.name}", debug)
+    except Exception as e:
+        error_msg(e)
 
 
-
+def delete_objs(conn, container_name, debug=False):
+    """Delete container objects"""
+    try:
+        objects = conn.list_objects(container_name)
+        info_msg(json.dumps(objects, indent=4), debug)
+        info_msg(f"Deleting objects from {container_name}", debug)
+        if objects:
+            for obj in objects:
+                conn.delete_object(container_name, str(obj.name))
+                info_msg(f"Deleted:     {obj.name}", debug)
+            success_msg(f"Objects have been deleted from {container_name}")
+    except Exception as e:
+        error_msg(e)
