51667e012e929a202cea5a42669dc1beb23c6086
Jonathan Race <jrace@augusta.edu>
2023-03-28 19:19:03 +0000

    chore: cleaned up excess utils and added enhancements to message capabilities

--- src/utils/msg_format.py
+++ src/utils/msg_format.py
@@ -4,33 +4,45 @@ def error_msg(error):
     if '\n' in error:
         # If error contains multiple lines, split it and print each line separately
         error_lines = error.split('\n')
-        print(Fore.RED + "ERROR: " + Fore.RESET)
+        print(Fore.RED + "[ERROR]   " + Fore.RESET)
         for line in error_lines:
             print(line)
     else:
         # If error is a single line, print it as is
-        print(Fore.RED + "ERROR: " + Fore.RESET + error)
+        print(Fore.RED + "[ERROR]   " + Fore.RESET + error)
 
 
-def info_msg(text):
-    if '\n' in text:
-        # If text contains multiple lines, split it and print each line separately
-        text_lines = text.split('\n')
-        print(Fore.BLUE + "INFO: " + Fore.RESET)
-        for line in text_lines:
-            print(line)
-    else:
-        # If text is a single line, print it as is
-        print(Fore.BLUE + "INFO: " + Fore.RESET + text)
+def info_msg(text, debug=False):
+    if debug:
+        if '\n' in text:
+            # If text contains multiple lines, split it and print each line separately
+            text_lines = text.split('\n')
+            print(Fore.BLUE + "[INFO]    " + Fore.RESET)
+            for line in text_lines:
+                print(line)
+        else:
+            # If text is a single line, print it as is
+            print(Fore.BLUE + "[INFO]    " + Fore.RESET + text)
 
 
 def success_msg(text):
     if '\n' in text:
         # If text contains multiple lines, split it and print each line separately
         text_lines = text.split('\n')
-        print(Fore.GREEN + "SUCCESS: " + Fore.RESET)
+        print(Fore.GREEN + "[SUCCESS] " + Fore.RESET)
         for line in text_lines:
             print(line)
     else:
         # If text is a single line, print it as is
-        print(Fore.GREEN + "SUCCESS: " + Fore.RESET + text)
+        print(Fore.GREEN + "[SUCCESS] " + Fore.RESET + text)
+
+def general_msg(text):
+    if '\n' in text:
+        # If text contains multiple lines, split it and print each line separately
+        text_lines = text.split('\n')
+        print(Fore.YELLOW + "[INFO]    " + Fore.RESET)
+        for line in text_lines:
+            print(line)
+    else:
+        # If text is a single line, print it as is
+        print(Fore.YELLOW + "[INFO]    " + Fore.RESET + text)
