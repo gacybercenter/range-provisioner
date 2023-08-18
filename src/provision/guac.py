d3b233ceb686e232e901ec1a12763f9bacc443b3
mccrusader <50123715+mccrusader@users.noreply.github.com>
2023-08-18 16:22:18 -0400

    Chore: merged an if and else statement.

--- src/provision/guac.py
+++ src/provision/guac.py
@@ -12,12 +12,13 @@ def provision(conn, gconn, globals, guacamole_globals, heat_params, debug=False)
                                 globals['range_name'], guacamole_globals['instance_mapping'], debug)
 
         openstack_instances = conn.list_servers()
-
+        
         if isinstance(globals['provision'], bool):
             if globals['provision']:
                 info_msg(
                     f"Global provisioning is set to: {globals['provision']}", debug)
                 general_msg("Provisioning Guacamole")
+                guac.
                 # provision = guac.provision(conn, name, dir, debug)
                 # if provision:
                 #     return provision
@@ -25,39 +26,38 @@ def provision(conn, gconn, globals, guacamole_globals, heat_params, debug=False)
                 general_msg("Deprovisioning Guacamole")
                 # deprovision = guac.deprovision(conn, name, debug)
                 # if deprovision:
-                #     return deprovision
-        else:
-            if isinstance(guacamole_globals['provision'], bool):
-                if guacamole_globals['provision']:
-                    info_msg(
-                        f"Swift provisioning is set to: {globals['provision']}", debug)
-                    if isinstance(guacamole_globals['update'], bool):
-                        if guacamole_globals['update']:
-                            info_msg(
-                                f"Swift update is set to: {globals['provision']}", debug)
-                            general_msg("Updating Guacamole")
-                            # deprovision = guac.deprovision(conn, name, debug)
-                            # provision = guac.provision(conn, name, dir, debug)
-                            # if deprovision and provision:
-                            #     return provision+deprovision
-                        else:
-                            general_msg("Provisioning Guacamole")
-                            # provision = guac.provision(conn, name, dir, debug)
-                            # if provision:
-                            #     return provision
+                #     return deprovisionScreen Recording
+        elif isinstance(guacamole_globals['provision'], bool):
+            if guacamole_globals['provision']:
+                info_msg(
+                    f"Swift provisioning is set to: {globals['provision']}", debug)
+                if isinstance(guacamole_globals['update'], bool):
+                    if guacamole_globals['update']:
+                        info_msg(
+                            f"Swift update is set to: {globals['provision']}", debug)
+                        general_msg("Updating Guacamole")
+                        # deprovision = guac.deprovision(conn, name, debug)
+                        # provision = guac.provision(conn, name, dir, debug)
+                        # if deprovision and provision:
+                        #     return provision+deprovision
                     else:
                         general_msg("Provisioning Guacamole")
                         # provision = guac.provision(conn, name, dir, debug)
                         # if provision:
                         #     return provision
-                elif not guacamole_globals['provision']:
-                    general_msg("Deprovisioning Guacamole")
-                    # deprovision = guac.deprovision(conn, name, debug)
-                    # if deprovision:
-                    #     return deprovision
-            else:
-                error_msg("Please check the provision parameter in globals.yaml")
-                return None
+                else:
+                    general_msg("Provisioning Guacamole")
+                    # provision = guac.provision(conn, name, dir, debug)
+                    # if provision:
+                    #     return provision
+            elif not guacamole_globals['provision']:
+                general_msg("Deprovisioning Guacamole")
+                # deprovision = guac.deprovision(conn, name, debug)
+                # if deprovision:
+                #     return deprovision
+        else:
+            error_msg("Please check the provision parameter in globals.yaml")
+            return None
     except Exception as e:
         error_msg(e)
 
