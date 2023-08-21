893412c791e456184a4c182f832f7e40e926369c
mccrusader <50123715+mccrusader@users.noreply.github.com>
2023-08-21 17:28:53 -0400

    Feature: Did a rough draft of guac provisioning

--- src/provision/guac.py
+++ src/provision/guac.py
@@ -1,25 +1,32 @@
-import orchestration.guac as guac
 import time
-from utils.generate import instances, users
+import orchestration.guac as guac
+from utils.generate import generate_instance_names, create_user_list
 from utils.msg_format import error_msg, info_msg, success_msg, general_msg
 
 
 def provision(conn, gconn, globals, guacamole_globals, heat_params, debug=False):
     try:
-        users_list = users(globals['num_ranges'], globals['num_users'],
-                                globals['range_name'], globals['user_name'], guacamole_globals['secure'], debug)
-        instances_list = instances(globals['num_ranges'], globals['num_users'],
-                                globals['range_name'], guacamole_globals['instance_mapping'], debug)
+        users_list = create_user_list(globals['num_ranges'],
+                                      globals['num_users'],
+                                      globals['range_name'],
+                                      globals['user_name'],
+                                      guacamole_globals['secure'],
+                                      debug)
+        instances_list = generate_instance_names(globals['num_ranges'],
+                                                 globals['num_users'],
+                                                 globals['range_name'],
+                                                 guacamole_globals['instance_mapping'],
+                                                 debug)
 
         openstack_instances = conn.list_servers()
-        
+
         if isinstance(globals['provision'], bool):
             if globals['provision']:
                 info_msg(
                     f"Global provisioning is set to: {globals['provision']}", debug)
                 general_msg("Provisioning Guacamole")
 
-                # provision = guac.provision(conn, name, dir, debug)
+                # provision(conn, gconn, globals, guacamole_globals, heat_params, users_list, instances_list, debug=False)
                 # if provision:
                 #     return provision
             elif not globals['provision']:
