967a4d9f30e3b0280d9478f526f73d2df6da7224
mccrusader <50123715+mccrusader@users.noreply.github.com>
2023-09-28 11:38:13 -0400

    chore: Improved the provision logic to only pull the required data.

--- src/provision/guac.py
+++ src/provision/guac.py
@@ -29,47 +29,6 @@ def provision(conn,
         None
     """
 
-    # Build the guac_params dictionary
-    guac_params = {}
-    guac_params['org_name'] = globals['org_name']
-    guac_params['protocol'] = heat_params['conn_proto']['default']
-    guac_params['password'] = heat_params['password']['default']
-    guac_params['username'] = heat_params['username']['default']
-    # Format the users.yaml data into groups and users data
-    if user_params:
-        guac_params['new_groups'] = format_groups(user_params,
-                                                  debug)
-        guac_params['new_users'] = format_users(user_params,
-                                                debug)
-    # If no users are specified, generate the groups and users data
-    else:
-        guac_params['new_groups'] = generate_groups(globals,
-                                                    debug)
-        guac_params['new_users'] = generate_users(globals,
-                                                  guacamole_globals,
-                                                  debug)
-    # Pull data from the Guacmole API
-    guac_params['domain_name'] = guac.find_domain_name(heat_params,
-                                                       debug)
-    guac_params['parent_group_id'] = guac.get_conn_group_id(gconn,
-                                                            guac_params['org_name'],
-                                                            debug)
-    guac_params['conn_groups'] = guac.get_groups(gconn,
-                                                 guac_params['parent_group_id'],
-                                                 debug)
-    guac_params['conn_group_ids'] = guac.find_group_ids(guac_params['conn_groups'],
-                                                        debug)
-    guac_params['conn_users'] = guac.get_users(gconn,
-                                               guac_params['org_name'],
-                                               debug)
-    guac_params['conn_list'] = guac.get_conns(gconn,
-                                              guac_params['conn_group_ids'],
-                                              debug)
-    # Pull data from the Heat API
-    guac_params['instances'] = heat.get_ostack_instances(conn,
-                                                         guac_params['new_groups'],
-                                                         debug)
-
     # Set the create and update flags from the globals vars
     if isinstance(globals['provision'], bool):
         create = globals['provision']
@@ -81,17 +40,73 @@ def provision(conn,
     elif isinstance(guacamole_globals['provision'], bool):
         create = guacamole_globals['provision']
         update = guacamole_globals['update']
+
+        if not create and update:
+            error_msg(
+                "Guacamole ERROR:  Can't have "
+                "provision: False, update: True in globals.yaml"
+            )
+            return
+
         info_msg("Guacamole provisioning is set to: "
                  f"{create}", debug)
         info_msg("Guacamole update is set to: "
                  f"{update}", debug)
 
     else:
-        error_msg("Please check the provision parameter in globals.yaml")
+        error_msg(
+            "Guacamole ERROR:  Please check the "
+            "provision parameter in globals.yaml"
+        )
         return
 
+    guac_params = {}
+
+    guac_params['org_name'] = globals['org_name']
+    guac_params['parent_group_id'] = guac.get_conn_group_id(gconn,
+                                                        guac_params['org_name'],
+                                                        debug)
+
+    # Populate the guac_params for provision or reprovision
+    if create or update:
+        guac_params['protocol'] = heat_params['conn_proto']['default']
+        guac_params['password'] = heat_params['password']['default']
+        guac_params['username'] = heat_params['username']['default']
+        # Format the users.yaml data into groups and users data
+        if user_params:
+            guac_params['new_groups'] = format_groups(user_params,
+                                                      debug)
+            guac_params['new_users'] = format_users(user_params,
+                                                    debug)
+        # If no users are specified, generate the groups and users data
+        else:
+            guac_params['new_groups'] = generate_groups(globals,
+                                                        debug)
+            guac_params['new_users'] = generate_users(globals,
+                                                      guacamole_globals,
+                                                      debug)
+        guac_params['domain_name'] = guac.find_domain_name(heat_params,
+                                                           debug)
+        guac_params['instances'] = heat.get_ostack_instances(conn,
+                                                             guac_params['new_groups'],
+                                                             debug)
+
+    # Populate the guac_params for deprovision or reprovision
+    if not create or update:
+        guac_params['conn_groups'] = guac.get_groups(gconn,
+                                                     guac_params['parent_group_id'],
+                                                     debug)
+        guac_params['conn_group_ids'] = guac.find_group_ids(guac_params['conn_groups'],
+                                                            debug)
+        guac_params['conn_users'] = guac.get_users(gconn,
+                                                   guac_params['org_name'],
+                                                   debug)
+        guac_params['conn_list'] = guac.get_conns(gconn,
+                                                  guac_params['conn_group_ids'],
+                                                  debug)
+
     # Provision, deprovision, or reprovision
-    if create and update:
+    if update:
         guac.reprovision(gconn,
                          guac_params,
                          debug)
