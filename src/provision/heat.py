5dfc8950771af52db6ca7304a5d9635b6dba75a9
mccrusader <50123715+mccrusader@users.noreply.github.com>
2023-10-02 16:59:36 -0400

    feat: Overhaulled the Heat provsion logic.

--- src/provision/heat.py
+++ src/provision/heat.py
@@ -1,173 +1,101 @@
-import orchestration.heat as heat
+"""
+Handles the logic for provisioning Heat
+"""
 import time
-from utils.msg_format import error_msg, info_msg, success_msg, general_msg
+from orchestration import heat
+from utils.msg_format import error_msg, info_msg, general_msg
+from utils.generate import generate_names
 
-def provision(conn, globals, heat_globals, heat_params, sec_params, debug=False):
-    try:
-        stack_name = globals['range_name']
-        instance_name = globals['user_name']
-        template_dir = heat_globals['template_dir']
+
+def provision(conn: object,
+              globals: dict,
+              heat_globals: dict,
+              heat_params: dict,
+              sec_params: dict,
+              debug=False) -> bool:
+    """ Provisions or deprovisions Heat based on the given parameters. """
+
+    # Set the create and update flags from the globals vars
+    if isinstance(globals['provision'], bool):
+        create = globals['provision']
+        update = False
+        info_msg("Global provisioning is set to: "
+                 f"{create}", debug)
+
+    # Set the create and update flags from the heat globals vars
+    elif isinstance(heat_globals['provision'], bool) and isinstance(heat_globals['update'], bool):
+        create = heat_globals['provision']
         update = heat_globals['update']
-        last_stack = False
-        if heat_params:
-            heat_params = {k: v['default'] for k, v in heat_params.items() if 'default' in v}
 
+        if not create and update:
+            error_msg(
+                "Heat ERROR:  Can't have "
+                "provision: False, update: True in globals.yaml"
+            )
+            return
+
+        info_msg("Heat provisioning is set to: "
+                 f"{create}", debug)
+        info_msg("Heat update is set to: "
+                 f"{update}", debug)
+
+    else:
+        error_msg(
+            "Heat ERROR:  Please check the Heat"
+            "provison and update parameters in globals.yaml"
+        )
+        return
+
+    # Format the parameters for Heat
+    if heat_params:
+        heat_params = {
+            k: v['default']
+            for k, v in heat_params.items()
+            if 'default' in v
+        }
+
+    if sec_params:
+        sec_params = {
+            k: v['default']
+            for k, v in sec_params.items()
+            if 'default' in v
+        }
+
+    heat_params['container_name'] = globals['range_name']
+    heat_params['instance_name'] = globals['user_name']
+    heat_params['count'] = globals['num_users']
+    stack_names = generate_names(globals['num_ranges'],
+                                 heat_params['container_name'])
+
+    # Provision, deprovision, or reprovision
+    if create:
         if sec_params:
-            sec_params = {k: v['default'] for k, v in sec_params.items() if 'default' in v}
-
-        if isinstance(globals['provision'], bool):
-            if globals['provision']:
-                info_msg(
-                    f"Global provisioning is set to: {globals['provision']}", debug)
-
-                if isinstance(heat_globals['sec'], bool):
-                    if heat_globals['sec']:
-                        dir = f"{template_dir}/sec.yaml"
-                        parameters = sec_params
-                        name = f"{stack_name}-sec"
-                        last_stack = True
-                        heat.provision(conn, name, dir, parameters,
-                                       last_stack, update, debug)
-                else:
-                    error_msg(
-                        "Please check the provision parameter in globals.yaml")
-                    return None
-
-                last_stack = False
-
-                if isinstance(heat_globals['main'], bool):
-                    if heat_globals['main']:
-                        dir = f"{template_dir}/main.yaml"
-                        parameters = heat_params
-                        parameters['container_name'] = stack_name
-                        parameters['instance_name'] = instance_name
-                        parameters['count'] = globals['num_users']
-
-                        if globals['num_ranges'] > 1:
-                            for i in range(globals['num_ranges']):
-                                time.sleep(heat_globals['pause'])
-                                name = f"{stack_name}{i+1}"
-                                if i == globals['num_ranges']-1:
-                                    last_stack = True
-                                heat.provision(
-                                    conn, name, dir, parameters, last_stack, update, debug)
-                        else:
-                            name = stack_name
-                            last_stack = True
-                            heat.provision(
-                                conn, name, dir, parameters, last_stack, update, debug)
-                else:
-                    error_msg(
-                        "Please check the provision parameter in globals.yaml")
-                    return None
-                return True
-            elif not globals['provision']:
-                if isinstance(heat_globals['main'], bool):
-                    if heat_globals['main']:
-                        if globals['num_ranges'] > 1:
-                            for i in range(globals['num_ranges']):
-                                time.sleep(heat_globals['pause'])
-                                name = f"{stack_name}{i+1}"
-                                if i == globals['num_ranges']-1:
-                                    last_stack = True
-                                heat.deprovision(
-                                    conn, name, last_stack, debug)
-                        else:
-                            name = stack_name
-                            last_stack = True
-                            heat.deprovision(conn, name, last_stack, debug)
-                else:
-                    error_msg(
-                        "Please check the provision parameter in globals.yaml")
-                    return None
-
-                if isinstance(heat_globals['sec'], bool):
-                    if heat_globals['sec']:
-                        name = f"{stack_name}-sec"
-                        last_stack = True
-                        heat.deprovision(conn, name, last_stack, debug)
-                else:
-                    error_msg(
-                        "Please check the provision parameter in globals.yaml")
-                    return None
-                return True
+            name = heat_params['container_name']
+            heat.provision(conn,
+                           f"{name}-sec",
+                           f"{heat_globals['template_dir']}/sec.yaml",
+                           sec_params,
+                           True,
+                           update,
+                           debug)
+            time.sleep(heat_globals['pause'])
         else:
-            if isinstance(heat_globals['provision'], bool):
-                if heat_globals['provision']:
-                    info_msg(
-                        f"Heat global provisioning is set to: {heat_globals['provision']}", debug)
-
-                    if isinstance(heat_globals['sec'], bool):
-                        if heat_globals['sec']:
-                            dir = f"{template_dir}/sec.yaml"
-                            parameters = sec_params
-                            name = f"{stack_name}-sec"
-                            last_stack = True
-                            heat.provision(conn, name, dir, parameters,
-                                        last_stack, update, debug)
-                    else:
-                        error_msg(
-                            "Please check the provision parameter in globals.yaml")
-                        return None
-
-                    last_stack = False
-
-                    if isinstance(heat_globals['main'], bool):
-                        if heat_globals['main']:
-                            dir = f"{template_dir}/main.yaml"
-                            parameters = heat_params
-                            parameters['container_name'] = stack_name
-                            parameters['instance_name'] = instance_name
-                            parameters['count'] = globals['num_users']
-
-                            if globals['num_ranges'] > 1:
-                                for i in range(globals['num_ranges']):
-                                    time.sleep(heat_globals['pause'])
-                                    name = f"{stack_name}{i+1}"
-                                    if i == globals['num_ranges']-1:
-                                        last_stack = True
-                                    heat.provision(
-                                        conn, name, dir, parameters, last_stack, update, debug)
-                            else:
-                                last_stack = True
-                                name = stack_name
-                                heat.provision(
-                                    conn, name, dir, parameters, last_stack, update, debug)
-                    else:
-                        error_msg(
-                            "Please check the provision parameter in globals.yaml")
-                        return None
-                    return True
-                elif not heat_globals['provision']:
-                    if isinstance(heat_globals['main'], bool):
-                        if heat_globals['main']:
-                            if globals['num_ranges'] > 1:
-                                for i in range(globals['num_ranges']):
-                                    time.sleep(heat_globals['pause'])
-                                    name = f"{stack_name}{i+1}"
-                                    if i == globals['num_ranges']-1:
-                                        last_stack = True
-                                    heat.deprovision(
-                                        conn, name, last_stack, debug)
-                            else:
-                                last_stack = True
-                                name = stack_name
-                                heat.deprovision(conn, name, last_stack, debug)
-                    else:
-                        error_msg(
-                            "Please check the provision parameter in globals.yaml")
-                        return None
-
-                    if isinstance(heat_globals['sec'], bool):
-                        if heat_globals['sec']:
-                            name = f"{stack_name}-sec"
-                            last_stack = True
-                            heat.deprovision(conn, name, last_stack, debug)
-                    else:
-                        error_msg(
-                            "Please check the provision parameter in globals.yaml")
-                        return None
-                    return True
-
-    except Exception as e:
-        error_msg(e)
+            general_msg("No security group parameters were provided")
+        for name in stack_names:
+            last_stack = name == stack_names[-1]
+            heat.provision(conn,
+                           name,
+                           f"{heat_globals['template_dir']}/main.yaml",
+                           heat_params,
+                           last_stack,
+                           update,
+                           debug)
+            time.sleep(heat_globals['pause'])
+    else:
+        for name in stack_names:
+            last_stack = name == stack_names[-1]
+            heat.deprovision(conn,
+                             name,
+                             last_stack,
+                             debug)
+            time.sleep(heat_globals['pause'])
