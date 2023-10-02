80e875c443250f07b9a15bfcc3fb7a7a552ad4f8
mccrusader <50123715+mccrusader@users.noreply.github.com>
2023-10-02 16:57:23 -0400

    feat: Cleaned up the heat functions.

--- src/orchestration/heat.py
+++ src/orchestration/heat.py
@@ -1,123 +1,159 @@
-
+"""
+Contains all the main functions for provisioning Heat
+"""
 from utils.msg_format import error_msg, info_msg, success_msg, general_msg
 
 
-def provision(conn, stack, template, parameters, last_stack=False, update_stack=False, debug=False):
+def provision(conn: object,
+              stack: str,
+              template: str,
+              parameters: dict,
+              last_stack=False,
+              update_stack=False,
+              debug=False) -> None:
     """Provision container and upload assets"""
-    try:
-        if update_stack:
-            update(conn, stack, template, parameters, last_stack, debug)
-        else:
-            create(conn, stack, template, parameters, last_stack, debug)
 
-    except Exception as e:
-        error_msg(e)
-        return None
+    if update_stack:
+        update(conn,
+               stack,
+               template,
+               parameters,
+               last_stack,
+               debug)
+    else:
+        create(conn,
+               stack,
+               template,
+               parameters,
+               last_stack,
+               debug)
 
 
-def deprovision(conn, stack, wait, debug=False):
+def deprovision(conn: object,
+                stack: str,
+                wait: bool,
+                debug=False) -> None:
     """Deprovision container and delete assets"""
-    try:
-        delete(conn, stack, wait, debug)
-    except Exception as e:
-        error_msg(e)
-        return None
+
+    delete(conn,
+           stack,
+           wait,
+           debug)
 
 
-def search(conn, stack_name, debug=False):
+def search(conn: object,
+           stack_name: str,
+           debug=False) -> list | None:
     """Search for a stack and return the stack if it exists."""
-    try:
-        general_msg(f"Searching for stack... {stack_name}")
-        result = conn.search_stacks(name_or_id=stack_name)
-        if result:
-            success_msg(f"{stack_name} stack exists")
-            return result
-        general_msg(f"{stack_name} stack doesn't exist")
-        return None
-    except Exception as e:
-        error_msg(e)
-        return None
+
+    general_msg(f"Heat:  Searching for stack... {stack_name}")
+    result = conn.search_stacks(name_or_id=stack_name)
+    if result:
+        success_msg(f"Heat:  {stack_name} stack exists")
+        info_msg(result, debug)
+        return result
+    general_msg(f"Heat:  {stack_name} stack doesn't exist")
+    return None
 
 
-def create(conn, stack, template, parameters, last_stack=False, debug=False):
+def create(conn: object,
+           stack: str,
+           template: str,
+           parameters: dict,
+           last_stack=False,
+           debug=False) -> None:
     """Create a new stack with the provided parameters."""
-    try:
-        exists = search(conn, stack, debug)
-        if exists:
-            error_msg(f"The stack {stack} already exists")
-            return None
-        else:
-            general_msg(f"Creating stack {stack}")
-            if parameters is None:
-                conn.create_stack(
-                    name=stack,
-                    template_file=template,
-                    wait=last_stack,
-                    rollback=False,
-                )
-            else:
-                conn.create_stack(
-                    name=stack,
-                    template_file=template,
-                    rollback=False,
-                    wait=last_stack,
-                    **parameters,
-                )
-            success_msg(f"The stack has been created:  {stack}")
-    except Exception as e:
-        error_msg(e)
+
+    exists = search(conn,
+                    stack,
+                    debug)
+    if exists:
+        error_msg(f"Heat:  The stack '{stack}' already exists")
         return None
 
-def update(conn, stack, template, parameters, last_stack=False, debug=False):
+    general_msg(f"Heat:  Creating stack '{stack}'")
+    if parameters is None:
+        conn.create_stack(
+            name=stack,
+            template_file=template,
+            wait=last_stack,
+            rollback=False,
+        )
+    else:
+        if 'name' in parameters.keys():
+            conn.create_stack(
+                template_file=template,
+                rollback=False,
+                wait=last_stack,
+                **parameters,
+            )
+        else:
+            conn.create_stack(
+                name=stack,
+                template_file=template,
+                rollback=False,
+                wait=last_stack,
+                **parameters,
+            )
+    success_msg(f"Heat:  Created '{stack}'")
+
+
+def update(conn: object,
+           stack: str,
+           template: str,
+           parameters: dict,
+           last_stack=False,
+           debug=False) -> None:
     """Update a deployed stack"""
-    try:
-        exists = search(conn, stack, debug)
-        if exists:
-            general_msg(f"Updating stack {stack}")
-            if parameters is None:
-                conn.update_stack(
-                    name_or_id=stack,
-                    template_file=template,
-                    wait=last_stack,
-                    rollback=False,
-                )
-            else:
-                conn.update_stack(
-                    name_or_id=stack,
-                    template_file=template,
-                    rollback=False,
-                    wait=last_stack,
-                    **parameters,
-                )
-            success_msg(f"The stack {stack} has been updated")
-        else:
-            error_msg(f"The stack {stack} cannot be updated, it doesn't exist")
-    except Exception as e:
-        error_msg(e)
-        return None
 
-def delete(conn, stack, wait, debug=False):
+    exists = search(conn, stack, debug)
+    if exists:
+        general_msg(f"Heat:  Updating stack '{stack}'")
+        if parameters is None:
+            conn.update_stack(
+                name_or_id=stack,
+                template_file=template,
+                wait=last_stack,
+                rollback=False,
+            )
+        else:
+            conn.update_stack(
+                name_or_id=stack,
+                template_file=template,
+                rollback=False,
+                wait=last_stack,
+                **parameters,
+            )
+        success_msg(f"Heat:  '{stack}' has been updated")
+    else:
+        error_msg(f"Heat:  '{stack}' cannot be updated, it doesn't exist")
+
+
+def delete(conn: object,
+           stack: str,
+           wait: bool,
+           debug=False) -> None:
     """Delete a deployed stack"""
-    try:
-        exists = search(conn, stack, debug)
-        if exists:
-            general_msg(f"Deleting stack {stack}")
-            conn.delete_stack(name_or_id=stack, wait=wait)
-            success_msg(f"The stack {stack} has been deleted")
-        else:
-            error_msg(f"The stack {stack} cannot be deleted, it doesn't exist")
-    except Exception as e:
-        error_msg(e)
-        return None
 
-def get_ostack_instances(conn,
-                         groups,
-                         debug=False):
+    exists = search(conn, stack, debug)
+    if exists:
+        general_msg(f"Heat:  Deleting stack '{stack}'")
+        conn.delete_stack(name_or_id=stack, wait=wait)
+        success_msg(f"Heat:  The stack '{stack}' has been deleted")
+    else:
+        error_msg(
+            f"Heat:  The stack '{stack}' cannot be deleted, it doesn't exist")
+
+
+def get_ostack_instances(conn: object,
+                         groups: list,
+                         debug=False) -> list:
     """
     Obtain openstack instance names and addresses
 
     Parameters:
         conn (object): The connection object for accessing the openstack instance.
+        groups (list): The list of groups to search for
         debug (bool, optional): A flag indicating whether to enable debug mode.
                                 Defaults to False.
 
@@ -137,6 +173,6 @@ def get_ostack_instances(conn,
         if instance['name'].split('.')[0] in groups
     ]
 
-    info_msg("Guacamole:  Retrieved OS Stack Instances:", debug)
+    general_msg(f"Heat:  Retrieved stack instances in {groups}")
     info_msg(instances, debug)
     return instances
