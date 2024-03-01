ac1a5635d9bf7c650cb63e371c2ceb32fe1d50e9
mccrusader <50123715+mccrusader@users.noreply.github.com>
2024-03-01 10:29:41 -0500

    chore: formatted the main function for readablility

--- src/provisioner.py
+++ src/provisioner.py
@@ -16,22 +16,35 @@ from provision import swift, heat, guac
 from utils import connections, load_template, manage_ids, msg_format
 
 def main() -> None:
+    """
+    The main function that handles the provisioning process based on command line arguments.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
     endpoint = 'Pipeline'
 
     try:
         # Parse and validate command line arguments
         arg = sys.argv[1:]
         if len(arg) == 0:
-            msg_format.error_msg("No arguments provided.", endpoint)
-            msg_format.general_msg("Valid arguments: 'swift', 'heat', 'guacamole', or 'full'", endpoint)
+            msg_format.error_msg("No arguments provided.",
+                                 endpoint)
+            msg_format.general_msg("Valid arguments: 'swift', 'heat', 'guacamole', or 'full'",
+                                   endpoint)
             return
 
         if arg[0] not in ["swift", "heat", "guacamole", "full"]:
-            msg_format.error_msg(f"'{arg[0]}' is an invalid arguement.", endpoint)
-            msg_format.general_msg("Valid arguments: 'swift', 'heat', 'guacamole', or 'full'", endpoint)
+            msg_format.error_msg(f"'{arg[0]}' is an invalid arguement.",
+                                 endpoint)
+            msg_format.general_msg("Valid arguments: 'swift', 'heat', 'guacamole', or 'full'",
+                                   endpoint)
             return
 
-        start = time.time()
+        start_time = time.time()
 
         global_dict = load_template.load_global()
         globals: Dict[str, Any] = global_dict['globals']
@@ -65,35 +78,83 @@ def main() -> None:
         msg_format.info_msg(json.dumps(env_params, indent=4), endpoint, debug)
         msg_format.info_msg(json.dumps(user_params, indent=4), endpoint, debug)
 
-        openstack_connect = connections.openstack_connection(heat_globals['cloud'], openstack_clouds, debug)
+        openstack_connect = connections.openstack_connection(heat_globals['cloud'],
+                                                             openstack_clouds,
+                                                             debug)
 
         if arg[0] == "swift":
-            swift.provision(openstack_connect, globals, swift_globals, debug)
+            swift.provision(openstack_connect,
+                            globals,
+                            swift_globals,
+                            debug)
         elif arg[0] == "heat":
             if env_params:
-                manage_ids.update_env(openstack_connect, global_dict, True, debug)
-                heat_params, sec_params, env_params = manage_ids.update_ids(openstack_connect, [heat_params, sec_params, env_params], [], False, debug)
-            heat.provision(openstack_connect, globals, heat_globals, heat_params, sec_params, debug)
+                manage_ids.update_env(openstack_connect,
+                                      global_dict,
+                                      True,
+                                      debug)
+                heat_params, sec_params, env_params = manage_ids.update_ids(openstack_connect,
+                                                                            [heat_params, sec_params, env_params],
+                                                                            [],
+                                                                            False,
+                                                                            debug)
+            heat.provision(openstack_connect,
+                           globals, heat_globals,
+                           heat_params,
+                           sec_params,
+                           debug)
         elif arg[0] == "guacamole":
-            guacamole_connect = connections.guacamole_connection(guacamole_clouds, debug)
-            guac.provision(openstack_connect, guacamole_connect, globals, guacamole_globals, heat_params, user_params, debug)
+            guacamole_connect = connections.guacamole_connection(guacamole_clouds,
+                                                                 debug)
+            guac.provision(openstack_connect,
+                           guacamole_connect,
+                           globals,
+                           guacamole_globals,
+                           heat_params,
+                           user_params,
+                           debug)
         elif arg[0] == "full":
-            guacamole_connect = connections.guacamole_connection(guacamole_clouds, debug)
-            swift.provision(openstack_connect, globals, swift_globals, debug)
+            guacamole_connect = connections.guacamole_connection(guacamole_clouds,
+                                                                 debug)
+            swift.provision(openstack_connect,
+                            globals,
+                            swift_globals,
+                            debug)
             if env_params:
-                manage_ids.update_env(openstack_connect, global_dict, True, debug)
-                heat_params, sec_params, env_params = manage_ids.update_ids(openstack_connect, [heat_params, sec_params, env_params], [], False, debug)
-            heat.provision(openstack_connect, globals, heat_globals, heat_params, sec_params, debug)
-            guac.provision(openstack_connect, guacamole_connect, globals, guacamole_globals, heat_params, user_params, debug)
+                manage_ids.update_env(openstack_connect,
+                                      global_dict,
+                                      True,
+                                      debug)
+                heat_params, sec_params, env_params = manage_ids.update_ids(openstack_connect,
+                                                                            [heat_params, sec_params, env_params],
+                                                                            [],
+                                                                            False,
+                                                                            debug)
+            heat.provision(openstack_connect,
+                           globals,
+                           heat_globals,
+                           heat_params,
+                           sec_params,
+                           debug)
+            guac.provision(openstack_connect,
+                           guacamole_connect,
+                           globals,
+                           guacamole_globals,
+                           heat_params,
+                           user_params,
+                           debug)
 
-        end = time.time()
-        msg_format.general_msg(f"Total time: {end - start:.2f} seconds", endpoint)
+        end_time = time.time()
+        msg_format.general_msg(f"Total time: {end_time - start_time:.2f} seconds",
+                               endpoint)
 
     except Exception as error:
-        msg_format.error_msg(f"Error: {error}.\nTraceback: {traceback.format_exc()}", endpoint)
+        msg_format.error_msg(f"Error: {error}.\n\n {traceback.format_exc()}",
+                             endpoint)
         sys.exit(1)
 
-    msg_format.success_msg("Provisioning complete.", endpoint)
+    msg_format.success_msg("Provisioning complete.",
+                           endpoint)
     sys.exit(0)
 
 
