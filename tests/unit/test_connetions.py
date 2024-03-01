062e951460e1476f20b3c9afff329fe3171b43ae
mccrusader <50123715+mccrusader@users.noreply.github.com>
2024-03-01 15:09:04 -0500

    fix: made the connection tests work as intended

--- tests/unit/test_connetions.py
+++ tests/unit/test_connetions.py
@@ -3,38 +3,168 @@
 """
 
 import unittest
-from unittest.mock import patch, mock_open
-from src.utils.load_template import load_template
+from unittest.mock import patch, MagicMock
+from src.utils.connections import openstack_connection, guacamole_connection
 
 class TestConnections(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    @patch('src.utils.connections.connect')
+    @patch('src.utils.connections.general_msg')
+    @patch('src.utils.connections.info_msg')
+    @patch('src.utils.connections.success_msg')
+    @patch('src.utils.connections.error_msg')
+    def test_openstack_connection_successful(self,
+                                             mock_error_msg,
+                                             mock_success_msg,
+                                             mock_info_msg,
+                                             mock_general_msg,
+                                             mock_connect):
+        """
+        Test for successful openstack connection.
 
-    @patch(
-        'src.utils.load_template.open',
-        new_callable=mock_open, read_data='name: Test\nvalue: 42'
-    )
-    @patch('src.utils.load_template.Munch')
-    @patch('src.utils.load_template.info_msg')
-    def test_open_connection(self, mock_info_msg, mock_munch, mock_file):
-        # Arrange
-        template_path = 'path/to/template.yaml'
-        mock_munch.return_value = {'name': 'Test', 'value': 42}
+        Args:
+            self: the object itself
+            mock_error_msg: MagicMock for error message patch
+            mock_success_msg: MagicMock for success message patch
+            mock_info_msg: MagicMock for info message patch
+            mock_general_msg: MagicMock for general message patch
+            mock_connect: MagicMock for connect patch
 
-        # Act
-        result = load_template(template_path)
+        Returns:
+            None
+        """
+        mock_connect.return_value = MagicMock()
 
-        # Assert
-        self.assertEqual(result, {'name': 'Test', 'value': 42})
-        mock_info_msg.assert_called_with(f"Loading {template_path}", 'Templates', False)
-        mock_file.assert_called_with(template_path, 'r', encoding='utf-8')
+        cloud = 'test_cloud'
+        openstack_clouds = {'auth': {'auth_url': 'http://example.com'}}
+        debug = True
 
+        result = openstack_connection(cloud, openstack_clouds, debug)
 
-    def tearDown(self):
-        pass
+        self.assertIsNotNone(result)
+        mock_success_msg.assert_called_once()
+        mock_error_msg.assert_not_called()
 
-# Add more test cases for other functions in a similar manner.
+    @patch('src.utils.connections.connect')
+    @patch('src.utils.connections.general_msg')
+    @patch('src.utils.connections.info_msg')
+    @patch('src.utils.connections.success_msg')
+    @patch('src.utils.connections.error_msg')
+    def test_openstack_connection_failure(self,
+                                          mock_error_msg,
+                                          mock_success_msg,
+                                          mock_info_msg,
+                                          mock_general_msg,
+                                          mock_connect):
+        """
+        Test for unsuccessful openstack connection.
+
+        Args:
+            self: the object itself
+            mock_error_msg: MagicMock for error message patch
+            mock_success_msg: MagicMock for success message patch
+            mock_info_msg: MagicMock for info message patch
+            mock_general_msg: MagicMock for general message patch
+            mock_connect: MagicMock for connect patch
+
+        Returns:
+            None
+        """
+        mock_connect.return_value = None
+
+        cloud = 'test_cloud'
+        openstack_clouds = {'auth': {'auth_url': 'http://example.com'}}
+        debug = True
+
+        result = openstack_connection(cloud, openstack_clouds, debug)
+
+        self.assertIsNone(result)
+        mock_error_msg.assert_called_once()
+        mock_success_msg.assert_not_called()
+
+    @patch('src.utils.connections.session')
+    @patch('src.utils.connections.general_msg')
+    @patch('src.utils.connections.info_msg')
+    @patch('src.utils.connections.success_msg')
+    @patch('src.utils.connections.error_msg')
+    def test_guacamole_connection_successful(self,
+                                             mock_error_msg,
+                                             mock_success_msg,
+                                             mock_info_msg,
+                                             mock_general_msg,
+                                             mock_session):
+        """
+        Test for successful guacamole connection.
+
+        Args:
+            self: the object itself
+            mock_error_msg: MagicMock for error message patch
+            mock_success_msg: MagicMock for success message patch
+            mock_info_msg: MagicMock for info message patch
+            mock_general_msg: MagicMock for general message patch
+            mock_connect: MagicMock for connect patch
+
+        Returns:
+            None
+        """
+        mock_session.return_value = MagicMock()
+
+        cloud = 'test_cloud'
+        guacamole_clouds = {
+            'host': 'guacamole.example.com',
+            'data_source': 'mysql',
+            'username': 'user',
+            'password': 'pass'
+        }
+        debug = True
+
+        result = guacamole_connection(cloud, guacamole_clouds, debug)
+
+        self.assertIsNotNone(result)
+        mock_success_msg.assert_called_once()
+        mock_error_msg.assert_not_called()
+
+    @patch('src.utils.connections.session')
+    @patch('src.utils.connections.general_msg')
+    @patch('src.utils.connections.info_msg')
+    @patch('src.utils.connections.success_msg')
+    @patch('src.utils.connections.error_msg')
+    def test_guacamole_connection_failure(self,
+                                          mock_error_msg,
+                                          mock_success_msg,
+                                          mock_info_msg,
+                                          mock_general_msg,
+                                          mock_session):
+        """
+        Test for successful openstack connection.
+
+        Args:
+            self: the object itself
+            mock_error_msg: MagicMock for error message patch
+            mock_success_msg: MagicMock for success message patch
+            mock_info_msg: MagicMock for info message patch
+            mock_general_msg: MagicMock for general message patch
+            mock_connect: MagicMock for connect patch
+
+        Returns:
+            None
+        """
+        mock_session.return_value = None
+
+        cloud = 'test_cloud'
+        guacamole_clouds = {
+            'host': 'guacamole.example.com',
+            'data_source': 'mysql',
+            'username': 'user',
+            'password': 'pass'
+        }
+        debug = True
+
+        result = guacamole_connection(cloud, guacamole_clouds, debug)
+
+        self.assertIsNone(result)
+        mock_error_msg.assert_called_once()
+        mock_success_msg.assert_not_called()
 
 if __name__ == '__main__':
     unittest.main()
