87ff9bdcc640b61a49f1b7c6689d6e2ce392351a
Marcus Corulli <mcorulli@augusta.edu>
2024-06-05 10:21:55 -0400

    chore: removed unhelpful comments from load templates unit tests

--- tests/unit/test_load_template.py
+++ tests/unit/test_load_template.py
@@ -18,14 +18,11 @@ class TestLoadTemplate(unittest.TestCase):
     @patch('src.utils.load_template.Munch')
     @patch('src.utils.load_template.info_msg')
     def test_load_template_success(self, mock_info_msg, mock_munch, mock_file):
-        # Arrange
         template_path = 'path/to/template.yaml'
         mock_munch.return_value = {'name': 'Test', 'value': 42}
 
-        # Act
         result = load_template(template_path)
-
-        # Assert
+        
         self.assertEqual(result, {'name': 'Test', 'value': 42})
         mock_info_msg.assert_called_with(f"Loading {template_path}", 'Templates', False)
         mock_file.assert_called_with(template_path, 'r', encoding='utf-8')
@@ -33,13 +30,10 @@ class TestLoadTemplate(unittest.TestCase):
     @patch('src.utils.load_template.open', side_effect=FileNotFoundError)
     @patch('src.utils.load_template.info_msg')
     def test_load_template_file_not_found(self, mock_info_msg, mock_file):
-        # Arrange
         template_path = 'path/to/nonexistent.yaml'
 
-        # Act
         result = load_template(template_path)
 
-        # Assert
         self.assertEqual(result, {})
         mock_info_msg.assert_called_with(f"Cannot find {template_path}", 'Templates', False)
         mock_file.assert_called_with(template_path, 'r', encoding='utf-8')
@@ -48,13 +42,10 @@ class TestLoadTemplate(unittest.TestCase):
     @patch('src.utils.load_template.error_msg')
     @patch('src.utils.load_template.info_msg')
     def test_load_template_generic_exception(self, mock_info_msg, mock_error_msg, mock_file):
-        # Arrange
         template_path = 'path/to/invalid.yaml'
 
-        # Act
         result = load_template(template_path)
 
-        # Assert
         self.assertEqual(result, {})
         mock_error_msg.assert_called_with("Cannot load template. Some error occurred", 'Templates')
         mock_info_msg.assert_called_with(f"Loading {template_path}", 'Templates', False)
@@ -64,13 +55,10 @@ class TestLoadTemplate(unittest.TestCase):
     @patch('src.utils.load_template.error_msg')
     @patch('src.utils.load_template.info_msg')
     def test_load_template_empty_exception(self, mock_info_msg, mock_error_msg, mock_file):
-        # Arrange
         template_path = 'path/to/empty.yaml'
 
-        # Act
         result = load_template(template_path)
 
-        # Assert
         self.assertEqual(result, {})
         mock_error_msg.assert_called_with("Cannot load template. Some error occurred", 'Templates')
         mock_info_msg.assert_called_with(f"Loading {template_path}", 'Templates', False)
