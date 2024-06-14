295b263ec6cb8a0f8395495fc8050c1ce89d32e0
Marcus Corulli <mcorulli@augusta.edu>
2024-06-14 15:03:05 -0400

    fix: unit tests for load template is now accurate

--- tests/unit/test_load_template.py
+++ tests/unit/test_load_template.py
@@ -1,73 +1,50 @@
-"""
-
-"""
-
 import unittest
 from unittest.mock import patch, mock_open
-from src.utils.load_template import load_template
+from src.utils import load_template
 
-class TestLoadTemplate(unittest.TestCase):
-
+class TestTemplateLoader(unittest.TestCase):
+    
     def setUp(self):
-        pass
+        self.template_content = 'key: value'
+        self.yaml_content = 'key: value'
+        self.template_name = 'template.j2'
+        self.yaml_file_name = 'main.yaml'
+        self.invalid_yaml_file_name = 'main.txt'
+        self.nonexistent_file_name = 'nonexistent.yaml'
 
-    @patch(
-        'src.utils.load_template.open',
-        new_callable=mock_open, read_data='name: Test\nvalue: 42'
-    )
-    @patch('src.utils.load_template.safe_load')
-    @patch('src.utils.load_template.info_msg')
-    def test_load_template_success(self, mock_info_msg, mock_safe_load, mock_file):
-        template_path = 'path/to/template.yaml'
-        mock_safe_load.return_value = {'name': 'Test', 'value': 42}
 
-        result = load_template(template_path)
-        
-        self.assertEqual(result, {'name': 'Test', 'value': 42})
-        mock_info_msg.assert_called_with(f"Loading {template_path}", 'Templates', False)
-        mock_file.assert_called_with(template_path, 'r', encoding='utf-8')
-
-    @patch('src.utils.load_template.open', side_effect=FileNotFoundError)
-    @patch('src.utils.load_template.info_msg')
-    def test_load_template_file_not_found(self, mock_info_msg, mock_file):
-        template_path = 'path/to/nonexistent.yaml'
-
-        result = load_template(template_path)
-
+    @patch('src.utils.load_template.load_template', return_value={'key': 'value'})
+    @patch('src.utils.load_template.os.path.exists', return_value=True)
+    @patch('src.utils.load_template.os.path.getsize', return_value=10)
+    def test_load_yaml_file_success(self, mock_getsize, mock_exists, mock_load_template):
+        result = load_template.load_yaml_file(self.yaml_file_name)
+        self.assertEqual(result, {'key': 'value'})
+    
+    @patch('src.utils.load_template.os.path.exists', return_value=True)
+    @patch('src.utils.load_template.os.path.getsize', return_value=0)
+    def test_load_yaml_file_empty(self, mock_getsize, mock_exists):
+        result = load_template.load_yaml_file(self.nonexistent_file_name)
         self.assertEqual(result, {})
-        mock_info_msg.assert_called_with(f"Cannot find {template_path}", 'Templates', False)
-        mock_file.assert_called_with(template_path, 'r', encoding='utf-8')
-
-    @patch('src.utils.load_template.open', side_effect=Exception('Some error occurred'))
-    @patch('src.utils.load_template.error_msg')
-    @patch('src.utils.load_template.info_msg')
-    def test_load_template_generic_exception(self, mock_info_msg, mock_error_msg, mock_file):
-        template_path = 'path/to/invalid.yaml'
-
-        result = load_template(template_path)
-
+    
+    def test_load_yaml_file_invalid_extension(self):
+        result = load_template.load_yaml_file(self.invalid_yaml_file_name)
         self.assertEqual(result, {})
-        mock_error_msg.assert_called_with("Cannot load template. Some error occurred", 'Templates')
-        mock_info_msg.assert_called_with(f"Loading {template_path}", 'Templates', False)
-        mock_file.assert_called_with(template_path, 'r', encoding='utf-8')
+    
+    @patch('src.utils.load_template.safe_load', return_value={'key': 'value'})
+    @patch('src.utils.load_template.Environment.get_template')
+    def test_load_template_success(self, mock_template, mock_safe_load):
+        mock_template.return_value.render.return_value = self.template_content
+        result = load_template.load_template(self.template_name)
+        self.assertEqual(result, {'key': 'value'})
+    
+    @patch('src.utils.load_template.Environment.get_template')
+    def test_load_template_syntax_error(self, mock_get_template):
+        mock_render = mock_get_template.return_value.render
+        mock_render.side_effect = load_template.TemplateSyntaxError(message='error', lineno=1)
+        with self.assertRaises(ValueError):
+            result = load_template.load_template(self.template_name)
+            self.assertEqual(result, {})
 
-    @patch('src.utils.load_template.open', side_effect=Exception('Some error occurred'))
-    @patch('src.utils.load_template.error_msg')
-    @patch('src.utils.load_template.info_msg')
-    def test_load_template_empty_exception(self, mock_info_msg, mock_error_msg, mock_file):
-        template_path = 'path/to/empty.yaml'
-
-        result = load_template(template_path)
-
-        self.assertEqual(result, {})
-        mock_error_msg.assert_called_with("Cannot load template. Some error occurred", 'Templates')
-        mock_info_msg.assert_called_with(f"Loading {template_path}", 'Templates', False)
-        mock_file.assert_called_with(template_path, 'r', encoding='utf-8')
-
-    def tearDown(self):
-        pass
-
-# Add more test cases for other functions in a similar manner.
 
 if __name__ == '__main__':
     unittest.main()
