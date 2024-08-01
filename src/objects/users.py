085f2aa3391631ca289a3937c0059a96e0f0a49c
MCcrusader <mcorulli@augusta.edu>
2024-08-01 15:57:41 -0400

    feat: Improved the mapping connections logic

--- src/objects/users.py
+++ src/objects/users.py
@@ -6,7 +6,7 @@ import re
 from time import sleep
 from secrets import choice
 from string import ascii_letters, digits
-from typing import List, Dict, Any
+from typing import List, Dict, Set, Tuple, Any
 import guacamole
 from objects.connections import ConnectionGroup, ConnectionInstance, SharingProfile, Connection
 from objects.parse import clean_dict, recursive_update
@@ -328,13 +328,23 @@ class NewUsers():
         self.gconn = gconn
         self.guac_data = guac_data
         self.organization = organization
-        self.connections: List[Connection] = connections or []
         self.users: List[User] = []
         self.defaults = guac_data.get('defaults', {})
         self.debug = debug
         self.current_users = CurrentUsers(
             gconn, organization, debug=debug
         ).users
+
+        self.connection_groups: List[ConnectionGroup] = []
+        self.connection_instances: List[ConnectionInstance] = []
+        self.sharing_profiles: List[SharingProfile] = []
+        for connection in connections:
+            if isinstance(connection, ConnectionGroup):
+                self.connection_groups.append(connection)
+            elif isinstance(connection, ConnectionInstance):
+                self.connection_instances.append(connection)
+            elif isinstance(connection, SharingProfile):
+                self.sharing_profiles.append(connection)
 
         self._create_users()
         self._update_passwords()
@@ -416,55 +426,83 @@ class NewUsers():
                             "Guacamole",
                             self.debug)
 
-    def _resolve_connections(self, names: list) -> tuple:
+    def _resolve_connections(self, names: List[str]) -> Tuple[List['ConnectionInstance'], List['ConnectionInstance'], List['SharingProfile']]:
         """
-        Resolve connection names to guacamole identifiers
+        Recursively search for connections based on a list of names
+        and return all parent groups in the hierarchy.
+
+        Parameters:
+        - names (list): The names of the connections to resolve.
+        
+        Returns:
+        - list: A list of all parent group ids in the hierarchy.
+        - list: A list of all connection ids in the hierarchy.
+        - list: A list of all sharing profile ids in the hierarchy.
         """
-        found_groups = set()
-        found_connections = set()
-        found_sharing_profiles = set()
 
-        for pattern_str in names:
-            pattern = re.compile(pattern_str)
-            for connection in self.connections:
-                if (isinstance(connection, ConnectionInstance)
-                    and pattern.search(connection.name)):
-                    found_connections.add(connection.identifier)
+        groups: Set[ConnectionGroup] = set()
+        connections: Set[ConnectionInstance] = set()
+        sharing_profiles: Set[SharingProfile] = set()
+        patterns = [re.compile(pattern_str) for pattern_str in names]
 
-        for connection in self.connections:
+        for connection in self.connection_instances:
+            if any(pattern.search(connection.name) for pattern in patterns):
+                connections.add(connection)
+
+        for connection in connections:
             parent = connection.parent_identifier
-            found_groups.update(self._resolve_groups(parent))
-            if (isinstance(connection, SharingProfile)
-                and parent in found_connections and connection.identifier):
-                found_sharing_profiles.add(connection.identifier)
+            identifier = connection.identifier
+            groups.update(self._resolve_groups(parent))
+            sharing_profiles.update(self._resolve_sharing_profiles(identifier))
 
-        return found_groups, found_connections, found_sharing_profiles
+        group_ids = [group.identifier for group in groups]
+        connection_ids = [connection.identifier for connection in connections]
+        sharing_profile_ids = [sharing_profile.identifier for sharing_profile in sharing_profiles]
 
-    def _resolve_groups(self, parent_identifier: str) -> set:
+        return group_ids, connection_ids, sharing_profile_ids
+
+    def _resolve_groups(self, parent_identifier: 'str') -> Set['ConnectionGroup']:
         """
         Recursively search for connection groups based on a parent identifier
         and return all parent groups in the hierarchy.
 
         Parameters:
-        - parent_identifier (str): The identifier of the parent group.
+        - parent_identifier (str): The the parent group to resolve.
 
         Returns:
-        - set: A set of all parent group identifiers in the hierarchy.
+        - set: A set of all parent groups in the hierarchy.
         """
 
         if parent_identifier == 'ROOT':
             return set()
 
-        found_groups = set()
-        for connection in self.connections:
-            if (isinstance(connection, ConnectionGroup) and
-                    connection.identifier == parent_identifier):
-                found_groups.add(connection.identifier)
-                found_groups.update(self._resolve_groups(
-                    connection.parent_identifier
-                ))
+        groups = set()
+        for connection in self.connection_groups:
+            if connection.identifier == parent_identifier:
+                groups.add(connection)
+                groups.update(self._resolve_groups(connection))
 
-        return found_groups
+        return groups
+
+    def _resolve_sharing_profiles(self, identifier: 'str') -> Set['SharingProfile']:
+        """
+        Recursively search for sharing profiles based on a parent
+        and return all sharing profiles in the hierarchy.
+
+        Parameters:
+        - identifier (str): The the connection to resolve.
+
+        Returns:
+        - set: A set of all sharing profiles in the hierarchy.
+        """
+
+        sharing_profiles = set()
+        for sharing_profile in self.sharing_profiles:
+            if sharing_profile.parent_identifier == identifier:
+                sharing_profiles.add(sharing_profile)
+
+        return sharing_profiles
+
 
     def _update_passwords(self) -> None:
         for new_user in self.users:
