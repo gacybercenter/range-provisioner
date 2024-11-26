6c74a2ec50cfbc1facbfba481f339d8a95431821
Marcus Corulli <mcorulli@augusta.edu>
2024-11-26 09:56:40 -0500

    chore: Adjusted the template loading messaging and formatting

--- src/utils/load_template.py
+++ src/utils/load_template.py
@@ -6,7 +6,8 @@ from jinja2 import Environment, FileSystemLoader, TemplateSyntaxError
 from yaml import safe_load
 from utils import msg_format
 
-def load_template(template_name: str,    
+
+def load_template(template_name: str,
                   template_dir: str = '.') -> dict | None:
     """
     Load a template file and return a dictionary
@@ -27,12 +28,14 @@ def load_template(template_name: str,
 
     except TemplateSyntaxError as error:
         error_message = error.message
-        raise ValueError(f"Template {template} has a syntax error: {error_message}")
+        raise ValueError(
+            f"Template {template} has a syntax error: {error_message}")
 
     except Exception as error:
         raise error
 
     return parameters
+
 
 def load_yaml_file(file_name: str,
                    template_dir: str | None = None,
@@ -51,7 +54,8 @@ def load_yaml_file(file_name: str,
     endpoint = 'Templates'
 
     if not file_name.split('.')[-1] in ['yaml', 'yml']:
-        msg_format.general_msg(f"'{file_name}' is not a YAML file", endpoint)
+        msg_format.general_msg(f"'{file_name}' is not a YAML file.",
+                               endpoint)
         return {}
 
     if template_dir:
@@ -61,20 +65,28 @@ def load_yaml_file(file_name: str,
         yaml_file_path = file_name
 
     if not os.path.exists(yaml_file_path):
-        msg_format.general_msg(f"Cannot find '{yaml_file_path}'", endpoint)
+        msg_format.general_msg(f"Cannot find file '{yaml_file_path}'.",
+                               endpoint)
         return {}
 
     if os.path.getsize(yaml_file_path) == 0:
-        msg_format.general_msg(f"The file '{yaml_file_path}' is empty", endpoint)
+        msg_format.general_msg(f"The file '{yaml_file_path}' is empty.",
+                               endpoint)
         return {}
 
-    msg_format.general_msg(f"Loading '{yaml_file_path}'", endpoint)
-    yaml_dict = load_template(file_name, template_dir)
+    msg_format.general_msg(f"Loading file '{yaml_file_path}'...",
+                           endpoint)
+    yaml_dict = load_template(file_name,
+                              template_dir)
 
     if yaml_dict:
-        msg_format.success_msg(f"'{yaml_file_path}' loaded", endpoint)
-        msg_format.info_msg(yaml_dict, endpoint, debug)
+        msg_format.success_msg(f"'{yaml_file_path}' loaded.",
+                               endpoint)
+        msg_format.info_msg(yaml_dict,
+                            endpoint,
+                            debug)
     else:
-        msg_format.error_msg(f"'{yaml_file_path}' not found", endpoint)
+        msg_format.error_msg(f"'{yaml_file_path}' not found.",
+                             endpoint)
 
     return yaml_dict
